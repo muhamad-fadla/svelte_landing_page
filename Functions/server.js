10a1c639fe80ed97e4ed8262b7fdebe499e38539
muhamad-fadla <yuratamma@gmail.com>
2021-10-10 10:02:35 +0700

    update: resolve issue

--- Functions/server.js
+++ Functions/server.js
@@ -3,15 +3,14 @@ const express = require('express');
 const app = express();
 const fs = require('fs')
 const path = require('path')
-const compression = require('compression');
 
 
 
-app.use(express.static(path.resolve('dist')))
+app.use(express.static(path.resolve('./../dist')))
 
 app.get('*', async (req,res) => {
 
-	const file = fs.readFileSync(path.resolve('dist/index.html'));
+	const file = fs.readFileSync(path.resolve('../dist/index.html'));
 
 	res.status(200).end(file);
 })
