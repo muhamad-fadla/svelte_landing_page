df64a5e6725e0c2b84d5bb98e481ff567d52f917
muhamad-fadla <yuratamma@gmail.com>
2021-10-10 10:33:57 +0700

    update: issue

---
+++ api/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+const fs = require('fs')
+const path = require('path') 
+
+app.use(express.static(path.join(__dirname, '/../dist')))
+
+let cached;
+
+app.get('*', async (req,res) => {
+	let file;
+
+	console.log(typeof cached == "undefined")
+
+	if(typeof cached == 'undefined'){
+		console.log('Storage cached')
+		file = fs.readFileSync(path.join(__dirname, '/../dist/index.html'));
+		cached = file;
+	}else{
+		console.log('use cached')
+		file = cached;
+	}
+
+
+	res.status(200).end(file);
+})
+
+
+const port = process.env.PORT || 3000;
+
+module.exports = server;
+// app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
